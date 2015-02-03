d0919631e4f9208a2ed00dccc5a04e637311490d
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-02-02 18:15:36 -0800

    Initial Commit

---
+++ MotionExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MotionExample
+//
+//  Created by Erick Bennett on 2/2/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MotionExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotionExample
+//
+//  Created by Erick Bennett on 2/2/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
